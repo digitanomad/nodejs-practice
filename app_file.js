8ecd2601a63d4fded317b00ce7adc95a5a4674fc
digitanomad <digitanomad@gmail.com>
2019-01-30 14:25:50 +0900

    파일 업로드

--- app_file.js
+++ app_file.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  });
+var upload = multer({ storage: _storage });
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
 app.set('views', './views_file');
 app.set('view engine', 'pug');
 
+app.get('/upload', function(req, res) {
+    res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res) {
+    console.log(req.file);
+    res.send('Uploaded : ' + req.file.filename);
+});
 app.get('/topic/new', function(req, res) {
     fs.readdir('data/', function(err, files) {
         if (err) {
