f8c65fe336056c97d1b7f6455f77a52eb42dc471
digitanomad <digitanomad@gmail.com>
2019-02-08 15:24:27 +0900

    cookie

--- app_cookie.js
+++ app_cookie.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
-app.use(cookieParser());
+app.use(cookieParser('SKEJRKDN@#!(92832'));
 
 var products = {
     1: {title: 'The history of web 1'},
@@ -18,15 +18,44 @@ app.get('/products', function(req, res) {
     }
     res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
 });
+app.get('/cart/:id', function(req, res) {
+    var id = req.params.id;
+    if (req.signedCookies.cart) {
+        var cart = req.signedCookies.cart;
+    } else {
+        var cart = {};
+    }
+    if (!cart[id]) {
+        cart[id] = 0;
+    }
+    cart[id] = parseInt(cart[id]) + 1;
+    res.cookie('cart', cart, {signed: true});
+    res.redirect('/cart');
+});
+app.get('/cart', function(req, res) {
+    var cart = req.signedCookies.cart;
+    if (!cart) {
+        res.send('Empty!');
+    } else {
+        var output = '';
+        for (var id in cart) {
+            output += `<li>${products[id].title} (${cart[id]})</li>`;
+        }
+    }
+
+    res.send(`<h1>Cart</h1>
+            <ul>${output}</ul>
+            <a href="/products">Products List</a>`);
+});
 app.get('/count', function(req, res) {
     
-    if (req.cookies.count) {
-        var count = parseInt(req.cookies.count);
+    if (req.signedCookies.count) {
+        var count = parseInt(req.signedCookies.count);
     } else {
         var count = 0;
     }
     count = count + 1;
-    res.cookie('count', count);
+    res.cookie('count', count, {signed: true});
     res.send('count : ' + count);
 });
 app.listen(3003, function() {
